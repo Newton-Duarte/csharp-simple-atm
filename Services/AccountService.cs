d14708daab502d531574d880f608cf5b8e60ef82
Newton Duarte Cavalcanti <newton_@hotmail.com>
2023-09-05 09:11:48 -0300

    feat: add deposit method to account service

--- Services/AccountService.cs
+++ Services/AccountService.cs
@@ -7,15 +7,29 @@ class AccountService
     _account = account;
   }
 
+  public void Deposit(double amount)
+  {
+    if (amount <= 0)
+    {
+      throw new AccountException("Amount must be greater than zero");
+    }
+
+    _account.Balance += amount;
+  }
+
   public void Withdraw(double amount)
   {
     if (_account.Balance < amount)
     {
-      throw new Exception("There's no enough balance");
+      throw new AccountException("There's no enough balance");
     }
     else if (amount <= 0)
     {
-      throw new ArgumentException("Amount must be greater than zero");
+      throw new AccountException("Amount must be greater than zero");
+    }
+    else if (amount > _account.WithdrawLimit)
+    {
+      throw new AccountException($"Amount must be less or equal to the withdraw limit of {_account.WithdrawLimit:F2}");
     }
 
     _account.Balance -= amount;
