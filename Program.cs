8512f129ee30f8b1231de2c0f4bc954989e46600
Newton Duarte Cavalcanti <newton_@hotmail.com>
2023-09-05 09:12:00 -0300

    feat: add try catch to main program

--- Program.cs
+++ Program.cs
@@ -1,9 +1,23 @@
-﻿System.Console.Write("Holder name: ");
-var name = Console.ReadLine();
+﻿try
+{
+  System.Console.Write("Holder name: ");
+  var name = Console.ReadLine();
 
-System.Console.Write("Pin: ");
-var pin = Console.ReadLine();
+  System.Console.Write("Pin: ");
+  var pin = Console.ReadLine();
 
-var account = new Account(name, pin);
+  var account = new Account(name, pin);
 
-System.Console.WriteLine(account);
+  System.Console.WriteLine(account);
+
+  var accountService = new AccountService(account);
+
+  accountService.Deposit(2000);
+  accountService.Withdraw(1000);
+
+  System.Console.WriteLine(account);
+}
+catch (Exception e)
+{
+  System.Console.WriteLine($"{e.GetType()}: {e.Message}");
+}
