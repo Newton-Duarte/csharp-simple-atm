22807de63796b06c11f1dfd65c8208858832ebf0
Newton Duarte Cavalcanti <newton_@hotmail.com>
2023-09-05 10:19:45 -0300

    feat: add main menu view to program

--- Program.cs
+++ Program.cs
@@ -1,21 +1,6 @@
 ﻿try
 {
-  System.Console.Write("Holder name: ");
-  var name = Console.ReadLine();
-
-  System.Console.Write("Pin: ");
-  var pin = Console.ReadLine();
-
-  var account = new Account(name, pin);
-
-  System.Console.WriteLine(account);
-
-  var accountService = new AccountService(account);
-
-  accountService.Deposit(2000);
-  accountService.Withdraw(1000);
-
-  System.Console.WriteLine(account);
+  MainMenuView.Show();
 }
 catch (Exception e)
 {
