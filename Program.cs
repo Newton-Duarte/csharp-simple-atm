81023830fb5c5d5652d980b9f62af123fc276333
Newton Duarte Cavalcanti <newton_@hotmail.com>
2023-09-05 06:11:52 -0300

    feat: add program

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿System.Console.Write("Holder name: ");
+var name = Console.ReadLine();
+
+System.Console.Write("Pin: ");
+var pin = Console.ReadLine();
+
+var account = new Account(name, pin);
+
+System.Console.WriteLine(account);
