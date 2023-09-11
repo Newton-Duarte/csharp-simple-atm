3a8d0f4ea4e86de83c8557b1a9230d2cce7e0e09
Newton Duarte Cavalcanti <newton_@hotmail.com>
2023-09-11 17:49:14 -0300

    feat: add success and error text util methods

--- Utils/TextUtils.cs
+++ Utils/TextUtils.cs
@@ -11,4 +11,20 @@ static class TextUtils
     Console.WriteLine(title);
     TextDivider();
   }
+
+  public static void Success(string text)
+  {
+    var defaultColor = Console.ForegroundColor;
+    Console.ForegroundColor = ConsoleColor.Green;
+    Console.WriteLine(text);
+    Console.ForegroundColor = defaultColor;
+  }
+
+  public static void Error(string text)
+  {
+    var defaultColor = Console.ForegroundColor;
+    Console.ForegroundColor = ConsoleColor.Red;
+    Console.WriteLine(text);
+    Console.ForegroundColor = defaultColor;
+  }
 }
